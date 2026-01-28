22078d1122c3f785756f9bc36ec827c34f0c0c0a
Lohith <sailohithkilli@gmail.com>
2026-01-28 13:41:50 +0530

    removed venv

--- venv/lib/python3.12/site-packages/pesuacademy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""PESU Academy package initialization."""
-
-__version__ = "1.0.0"
-
-from .pesuacademy import PESUAcademy
-
-__all__ = ["PESUAcademy"]
--- venv/lib/python3.12/site-packages/pesuacademy/util/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions for PESU Academy package."""
-
-from .utils import _build_params
-
-__all__ = ["_build_params"]
